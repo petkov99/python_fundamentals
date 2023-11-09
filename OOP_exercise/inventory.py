20eb98efc112f1427b44d6393d950b26f365b551
petkov99 <145151374+petkov99@users.noreply.github.com>
2023-11-09 20:13:06 +0200

    solve for problem #7

--- OOP_exercise/inventory.py
+++ OOP_exercise/inventory.py
@@ -12,11 +12,10 @@ class Inventory:
             return "not enough room in the inventory"
 
     def get_capacity(self):
-        return self.__capacity
+        return len(self.items)
 
     def __repr__(self):
         return f"Items: {', '.join(self.items)}.\nCapacity left: {self.__capacity}"
-
 
 inventory = Inventory(2)
 inventory.add_item("potion")
