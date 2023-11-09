44bb8d8094ba6790b609af999b9038b66a9cf0f0
petkov99 <145151374+petkov99@users.noreply.github.com>
2023-11-09 20:25:37 +0200

    solve for problem #8 #9

--- OOP_exercise/vehicle.py
+++ OOP_exercise/vehicle.py
@@ -1,16 +1,16 @@
 class Vehicle:
 
-    def __init__(self, type, model, price):
+    def __init__(self, type:str, model:str, price:int):
         self.type = type
         self.model = model
         self.price = price
         self.owner = None
 
     def buy(self, money: int, owner: str):
-        if money > self.price and self.owner is None:
+        if money >= self.price and self.owner is None:
             change = money - self.price
             self.owner = owner
-            return f"Successfully bought a {self.type}. Change: {change}"
+            return f"Successfully bought a {self.type}. Change: {change:.2f}"
 
         elif money < self.price:
             return "Sorry, not enough money"
